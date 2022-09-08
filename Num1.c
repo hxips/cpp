1231326b6bfcb9143aebdb03acd8c136b715b8b2
hxips <101568562+hxips@users.noreply.github.com>
2022-09-08 18:52:16 +0300

    Add files via upload

---
+++ Num1.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    scanf_s("%i", &a);
+
+    if (a % 2 == 0 && ((a <= 20 && a >= 0) || (a < 200 && a > 100)))
+    {
+        printf("Yes");
+    }
+    else
+        printf("No");
+}
