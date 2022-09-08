dd80ece9d87876c816f4d1fc74026a176d4c0d4f
hxips <101568562+hxips@users.noreply.github.com>
2022-09-08 18:56:27 +0300

    Add files via upload

---
+++ 1HW/Num6.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+
+int main()
+{
+    int a, b;
+    scanf_s("%i %i", &a, &b);
+
+    int l = 1, n = a;
+
+    for (int i = a; i <= b; ++i)
+    {
+        int j = i, Len = 1;
+
+        while (j != 1)
+        {
+            if (j % 2 == 0)
+            {
+                j /= 2;
+            }
+
+            else
+            {
+                j = 3 * j + 1;
+            }
+            ++Len;
+        }
+
+        if (Len > l)
+        {
+            l = Len;
+            n = i;
+        }
+        Len = 1;
+    }
+
+    printf("Number = %i, Lenght = %i", n, l);
+}
---
+++ 1HW/Num8.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf_s("%i %i %i", &a, &b, &c);
+    int j;
+
+    j = a + c - (a % c);
+
+    for (j; j <= b; j += c)
+        printf("%i ", j);
+}
