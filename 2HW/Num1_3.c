89e29423a9dcecc084923ba16fb57924991d444d
hxips <101568562+hxips@users.noreply.github.com>
2022-09-08 23:04:44 +0300

    Add files via upload

---
+++ 2HW/Num1_3.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[1000];
+	int n;
+	scanf_s("%i", &n);
+	for (int i = 0; i < n; ++i)
+		scanf_s("%i", &a[i]);
+
+	int j;
+	scanf_s("%i", &j);
+
+	for (int i = j; i < n - 1; ++i)
+		a[i] = a[i + 1];
+
+	for (int i = 0; i < n - 1; ++i)
+		printf("%i ", a[i]);
+	printf("\n");
+}
