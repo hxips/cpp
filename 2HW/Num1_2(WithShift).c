4a186c7abf457962dc33563873a7bdc61abffba7
hxips <101568562+hxips@users.noreply.github.com>
2022-09-08 22:48:10 +0300

    Add files via upload

---
+++ 2HW/Num1_2(WithShift).c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[1000];
+	int n;
+	scanf_s("%i", &n);
+
+	for (int i = 0; i < n; ++i)
+		scanf_s("%i", &a[i]);
+
+	int j, num;
+	scanf_s("%i %i", &num, &j);
+
+	for (int i = n; i > j; --i)
+	{
+		a[i] = a[i - 1];
+	}
+	a[j + 1] = num;
+
+	for (int i = 0; i <= n; ++i)
+		printf("%i ", a[i]);
+	printf("\n");
+}
