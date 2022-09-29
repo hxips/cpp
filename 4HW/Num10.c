4b996fcda38e082477d83f3355c1d4b687678b5c
hxips <101568562+hxips@users.noreply.github.com>
2022-09-30 01:36:45 +0300

    Add files via upload

---
+++ 4HW/Num10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("char:\t%i\n", sizeof(char));
+
+    printf("short:\t%i\n", sizeof(short));
+
+    printf("int:\t%i\n", sizeof(int));
+
+    printf("long long:\t%i\n", sizeof(long long));
+
+    printf("size_t:\t%i\n", sizeof(size_t));
+
+    printf("float:\t%i\n", sizeof(float));
+
+    printf("double:\t%i\n", sizeof(double));
+
+    printf("int*:\t%i\n", sizeof(int*));
+
+    int arr[10];
+    printf("int[10]:\t%i\n", sizeof(arr));
+}
